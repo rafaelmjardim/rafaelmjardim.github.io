a093789d3d9d19a2c17f63ff753cae4ae0ba1d21
rafaelmjardim <rafaelmjardim@gotmail.com>
2022-05-20 22:31:20 -0300

    Correção nos alertas

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,7 @@
 var alerta = document.getElementById('alerta')
+var alerta2 = document.getElementById('alerta2')
 alerta.addEventListener('click', clicar)
+alerta2.addEventListener('click', clicar)
 function clicar() {
     alert("Em desenvolvimento!")
 }

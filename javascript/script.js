d806a5d86ae276bed8025b60e3c07c46a8aa51aa
rafaelmjardim <rafaelmjardim@gotmail.com>
2022-06-01 23:28:02 -0300

    Nav Bar responsive

--- javascript/script.js
+++ javascript/script.js
@@ -2,6 +2,17 @@ var alerta = document.getElementById('alerta')
 var alerta2 = document.getElementById('alerta2')
 alerta.addEventListener('click', clicar)
 alerta2.addEventListener('click', clicar)
+
 function clicar() {
     alert("Em desenvolvimento!")
+}
+
+function navOpen() {
+    var navOpen = document.getElementById('nav-mobile');
+    navOpen.style.width = '100%';
+}
+
+function navClose() {
+    var navClose = document.getElementById('nav-mobile');
+    navClose.style.width = '0';
 }
